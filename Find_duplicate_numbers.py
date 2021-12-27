a433469832ce577c6e9f214461b55d550b929b5f
james-monahan <45875322+james-monahan@users.noreply.github.com>
2021-12-27 09:40:49 +0100

    Update Find_duplicate_numbers.py

--- Find_duplicate_numbers.py
+++ Find_duplicate_numbers.py
@@ -1,4 +1,6 @@
-
+# Problem statement
+# Write a method to find duplicate numbers in an integer array. 
+# This method should return the duplicate numbers as a sorted array of unique integers.
 
 from typing import List
 
